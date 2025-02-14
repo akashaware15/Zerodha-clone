0eb350634ef2b5ce5a22718431f3f8bd695dd120
Akash Aware <akashaware90@gmail.com>
2025-02-14 18:55:40 +0530

    Add Some Text

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('fa-x');
     aboutmenu.classList.toggle('show');
 }
+
+
+function showMessage() {
+    let confirmAction = confirm("Are you sure you want to go?");
+      if (confirmAction) 
+    window.location.href = " https://akashaware15.github.io/Sign-Up-page/";
+}
