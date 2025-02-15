5568a871f9abac2830683c41ba9672b9f7b377a5
Akash Aware <163508167+akashaware15@users.noreply.github.com>
2025-02-15 14:20:46 +0530

    Delete signup-page directory

--- signup-page/script.js
+++
@@ -1,7 +0,0 @@
-let menuIcon = document.querySelector('#menu-icon');
-let aboutmenu = document.querySelector('.about-menu');
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('fa-x');
-    aboutmenu.classList.toggle('show');
-}
