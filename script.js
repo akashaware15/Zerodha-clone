88afd23a4ea3b22077549187370b03d3f452a90d
Akash Aware <akashaware90@gmail.com>
2025-02-15 15:27:20 +0530

    Add Some File

--- script.js
+++ script.js
@@ -5,8 +5,3 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('fa-x');
     aboutmenu.classList.toggle('show');
 }
-
-
-function showMessage() {
-    window.location.href = " https://akashaware15.github.io/Sign-Up-page/";
-}
