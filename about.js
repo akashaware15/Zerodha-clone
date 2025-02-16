ba86434f88ce996136936b87fcb39331ccd2b0dd
Akash Aware <akashaware90@gmail.com>
2025-02-16 14:45:20 +0530

    Add Some Style

--- about.js
+++ about.js
@@ -5,3 +5,11 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('fa-x');
     aboutmenu.classList.toggle('show');
 }
+
+const cards = document.querySelectorAll('.member-card');
+
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            card.classList.toggle('active');
+        });
+});
